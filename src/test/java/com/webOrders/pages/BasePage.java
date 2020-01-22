994c3acd2a551bb28146a512ce70e325ca387694
Saadet Akgobek <saadetdemirtas620@gmail.com>
2020-01-22 00:01:29 -0600

    cucumberrunneradded

--- src/test/java/com/webOrders/pages/BasePage.java
+++ src/test/java/com/webOrders/pages/BasePage.java
@@ -1,4 +1,37 @@
 package com.webOrders.pages;
 
-public class BasePage {
+import com.webOrders.utilities.BrowserUtils;
+import com.webOrders.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public abstract class BasePage {
+
+    @FindBy(tagName = "h1")
+    private WebElement pageLogo;
+
+    @FindBy(tagName = "th")
+    private List<WebElement> columnNamesElements;
+
+    public BasePage() {
+        //it's coming from selenium webdriver
+        //to enable @FindBy annotation
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    public String getPageLogoText() {
+        return pageLogo.getText();
+    }
+
+    public void navigateTo(String moduleName) {
+        Driver.get().findElement(By.linkText(moduleName)).click();
+    }
+
+    public List<String> getColumnNames() {
+        return BrowserUtils.getListOfString(columnNamesElements);
+    }
 }
